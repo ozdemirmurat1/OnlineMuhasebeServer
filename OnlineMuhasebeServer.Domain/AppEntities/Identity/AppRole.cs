8767da896c7caf6b11fac48b816d65b0c7d5c575
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-07-21 15:45:14 +0300

    Commit

---
+++ OnlineMuhasebeServer.Domain/AppEntities/Identity/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Domain.AppEntities.Identity
+{
+    public sealed class AppRole:IdentityRole<string>
+    {
+    }
+}
---
+++ OnlineMuhasebeServer.Persistence/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Persistence.Context
+{
+    public sealed class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
