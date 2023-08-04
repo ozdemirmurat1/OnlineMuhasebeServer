09ed1912a475101abb33ca8c6284b51100ad4af3
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-08-04 19:35:31 +0300

    Commit

--- OnlineMuhasebeServer.Domain/AppEntities/Identity/AppRole.cs
+++ OnlineMuhasebeServer.Domain/AppEntities/Identity/AppRole.cs
@@ -9,6 +9,17 @@ namespace OnlineMuhasebeServer.Domain.AppEntities.Identity
 {
     public sealed class AppRole:IdentityRole<string>
     {
+        public AppRole()
+        {
+            
+        }
+        public AppRole(string title,string code,string name)
+        {
+            Code = code;
+            Name = name;
+            Title = title;
+        }
         public string Code { get; set; }
+        public string Title { get; set; }
     }
 }
---
+++ OnlineMuhasebeServer.Domain/Roles/RoleList.cs
@@ -0,0 +1,53 @@
+﻿using OnlineMuhasebeServer.Domain.AppEntities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineMuhasebeServer.Domain.Roles
+{
+    public sealed class RoleList
+    {
+        public static List<AppRole> GetStaticRoles()
+        {
+            List<AppRole> appRoles = new List<AppRole>
+            {
+                new AppRole(
+                    title: UCAF,
+                    code: UCAFCreateCode,
+                    name: UCAFCreateName),
+
+                new AppRole(
+                    title: UCAF,
+                    code: UCAFUpdateCode,
+                    name: UCAFUpdateName
+                    )
+            };
+
+            return appRoles;
+        }
+
+        #region RoleTitleName
+        public static string UCAF = "Hesap Planı";
+        #endregion
+
+        #region RoleCodeAndNames
+        public static string UCAFCreateCode = "UCAF.Create";
+        public static string UCAFCreateName = "Hesap Planı Kayıt";
+
+        public static string UCAFUpdateCode = "UCAF.Update";
+        public static string UCAFUpdateName = "Hesap Planı Güncelle";
+
+        public static string UCAFRemoveCode = "UCAF.Remove";
+        public static string UCAFRemoveName = "Hesap Planı Sil";
+
+        public static string UCAFReadCode = "UCAF.Read";
+        public static string UCAFReadName = "Hesap Planı Görüntüleme";
+        #endregion
+
+
+
+
+    }
+}
