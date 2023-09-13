d3c6d11fe5b1a8de6c4316e174f48a27f97db4aa
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-09-13 11:19:36 +0300

    Commit

--- OnlineMuhasebeServer.Persistence/Services/CompanyServices/ReportService.cs
+++ OnlineMuhasebeServer.Persistence/Services/CompanyServices/ReportService.cs
@@ -1,10 +1,25 @@
 ﻿using OnlineMuhasebeServer.Application.Services.CompanyService;
+using OnlineMuhasebeServer.Domain;
 using OnlineMuhasebeServer.Domain.CompanyEntities;
+using OnlineMuhasebeServer.Domain.Repositories.CompanyDbContext.ReportRepositories;
+using OnlineMuhasebeServer.Persistence.Context;
 
 namespace OnlineMuhasebeServer.Persistence.Services.CompanyServices
 {
     public class ReportService : IReportService
     {
+        private CompanyDbContext _context;
+        private readonly IContextService _contextService;
+        private readonly IReportCommandRepository _commandRepository;
+        private readonly IReportQueryRepository _queryRepository;
+
+        public ReportService(IContextService contextService, IReportCommandRepository commandRepository, IReportQueryRepository queryRepository)
+        {
+            _contextService = contextService;
+            _commandRepository = commandRepository;
+            _queryRepository = queryRepository;
+        }
+
         public Task<IList<Report>> GetAllReportsByCompanyId(string companyId)
         {
             throw new NotImplementedException();
