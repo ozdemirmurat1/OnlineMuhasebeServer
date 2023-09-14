4b64b568f5dd7ec9538acf5593a74100dc1a32b6
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2023-09-14 23:23:09 +0300

    Commit

---
+++ OnlineMuhasebeServer.Application/Services/IRabbitMQService.cs
@@ -0,0 +1,9 @@
+﻿using OnlineMuhasebeServer.Domain.CompanyEntities;
+
+namespace OnlineMuhasebeServer.Application.Services
+{
+    public interface IRabbitMQService
+    {
+        void SendQueue(Report report, string companyId);
+    }
+}
